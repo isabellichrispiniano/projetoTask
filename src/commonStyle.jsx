68103194bb338b35fce09f6173b6ebb44b0e18a7
isabellichrispiniano <chrispinianoisabelli@gmail.com>
2024-06-10 11:45:05 -0300

    adicionando o delete para deletar tarefas ja concluidas

--- src/commonStyle.jsx
+++ src/commonStyle.jsx
@@ -1,6 +1,7 @@
 export default {
     fontFamily: 'Lato',
     colors:{
+        today:'#C40C0C',
         secundary:"#FFF",
         mainText:'#222',
         subText:'#555',
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,98 +1,144 @@
+
 import React from "react";
-import { View, Text, StyleSheet,  TouchableWithoutFeedback } from 'react-native';
-
-import commonStyle from "../commonStyle";
+import { View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity } from "react-native";
+import { GestureHandlerRootView, Swipeable } from "react-native-gesture-handler";
+ 
+import commonStyles from "../commonStyle";
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+ 
 import moment from "moment";
 import 'moment/locale/pt-br'
-
-
-
+ 
 export default props => {
-    const tarefaNaoConcluida = props.concluidaEm != null ?
+ 
+    const tarefaConcluidaNao = props.concluidaEm != null ?
         { textDecorationLine: 'line-through' } : {}
-
+ 
     const date = props.concluidaEm ? props.concluidaEm : props.dataEstimada
-
+ 
     const dateFormat = moment(date).locale('pt-br')
         .format('ddd, D [de] MMMM')
-
-
-    return (
-        <View style={style.container} >
-            <TouchableWithoutFeedback
-                onPress={() => props.toggleTask(props.id)}
+ 
+    const getRightContent = () => {
+        return (
+            <TouchableOpacity style={style.right}
+                onPress={() => props.onDelete && props.onDelete(props.id)}
             >
-
-            < View style={style.checkContainer} >
-                {getCheckView(props.concluidaEm)}
+                <Icon name='trash' size={30} color='#FFF' />
+            </TouchableOpacity>
+        )
+    }
+    const getLeftContent = () => {
+        return (
+            <View style={style.left}>
+                <Icon name='trash' size={20} color='#FFF' style={style.delIcon} />
+                <Text style={style.delText}>Excluir</Text>
             </View>
-            </TouchableWithoutFeedback>
-
-
-            <View>
-                <Text style={[style.descricao, tarefaNaoConcluida]} >{props.descricao}</Text>
-                <Text style={style.date} >{dateFormat}</Text>
-            </View>
-        </View>
+        )
+    }
+ 
+    return (
+        <GestureHandlerRootView>
+            <Swipeable
+                renderRightActions={getRightContent}
+                renderLeftActions={getLeftContent}
+                onSwipeableLeftOpen={() => props.onDelete && props.onDelete(props.id)}
+            >
+                <View style={style.container}>
+                    <TouchableWithoutFeedback
+                        onPress={() => props.toggleTask(props.id)}
+                    >
+                        <View style={style.checkContainer}>
+                            {getCheckView(props.concluidaEm)}
+                        </View>
+                    </TouchableWithoutFeedback>
+                    <View>
+                        <Text style={[style.descricao, tarefaConcluidaNao]}>{props.descricao}</Text>
+                        <Text style={style.date}>{dateFormat}</Text>
+                    </View>
+                </View>
+            </Swipeable>
+        </GestureHandlerRootView>
     )
 }
-
+ 
 function getCheckView(concluidaEm) {
     if (concluidaEm != null) {
         return (
-            <View style={style.dataEstimada} >
+            <View style={style.dataEstimada}>
                 <Icon name='check' size={20} color='#FFF' />
             </View>
         )
     } else {
         return (
-            <View style={style.pendente} >
-
+            <View style={style.pendente}>
+ 
             </View>
         )
     }
 }
-
-const style = StyleSheet.create(
-    {
-        container: {
-            flexDirection: 'row',
-            borderColor: '#2c2c2c',
-            borderBottomWidth: 1,
-            alignItems: 'center',
-            paddingVertical: 10,
-        },
-        pendente: {
-            height: 25,
-            width: 25,
-            borderRadius: 13,
-            borderWidth: 1,
-            borderColor: '#555',
-        },
-        dataEstimada: {
-            height: 25,
-            width: 25,
-            borderRadius: 13,
-            borderWidth: 1,
-            backgroundColor: '#4D7031',
-            alignItems: "center",
-            justifyContent: "center",
-        },
-        descricao: {
-            fontSize: 15,
-            color: commonStyle.colors.mainText,
-        },
-        date: {
-            fontFamily: commonStyle.fontFamily,
-            color: commonStyle.colors.subText,
-            fontSize: 12,
-        },
-        checkContainer: {
-            width: '20%',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
+ 
+const style = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        borderColor: '#2c2c2c',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        paddingVertical: 10,
+        backgroundColor: '#FFF'
+    },
+    checkContainer: {
+        width: '20%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    pendente: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        borderColor: '#555',
+    },
+    dataEstimada: {
+        height: 25,
+        width: 25,
+        borderRadius: 13,
+        borderWidth: 1,
+        backgroundColor: '#4D7031',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    descricao: {
+        color: commonStyles.colors.mainText,
+        fontSize: 15,
+    },
+    date: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.subText,
+        fontSize: 12,
+ 
+    },
+    right: {
+        paddingHorizontal: 20,
+        backgroundColor: 'red',
+        justifyContent: 'flex-end',
+        flexDirection: 'row',
+        alignItems: 'center',
+ 
+    },
+    left: {
+        flex: 1,
+        backgroundColor: 'red',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    delText: {
+        fontFamily: commonStyles.fontFamily,
+        color: '#FFF',
+        fontSize: 20,
+        margin: 10,
+    },
+    delIcon: {
+        marginLeft: 10,
     }
-)
+})
